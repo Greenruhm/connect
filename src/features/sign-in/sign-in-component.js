086c438c6fbc0b216999852d77d363ef30f592b7
Oliver Day <oliver.day.swe@gmail.com>
2022-08-28 20:05:52 -0700

    feat: add styled '/sign-in' page

---
+++ src/features/sign-in/sign-in-component.js
@@ -0,0 +1,197 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import connect from '../../index';
+
+const color = {
+  primary: '#0A0C1F',
+  secondary: '#5AE6C1',
+  button: '#49ECBD',
+};
+
+const { signIn } = connect({ apiKey: 'test_success' });
+
+const SignInPage = () => {
+  const [state, setState] = useState({
+    email: '',
+    errors: [],
+  });
+  const { errors } = state;
+
+  const handleEmail = e => {
+    setState(state => ({
+      ...state,
+      email: e.target.value,
+    }));
+  };
+
+  const handleSignIn = async () => {
+    try {
+      await signIn({ email: state.email, username: state.username });
+    } catch (e) {
+      setState(state => ({
+        ...state,
+        errors: [...state.errors, e.message],
+      }));
+    }
+  };
+
+  return (
+    <>
+      <div className="box-format font-format">
+        <div className="sign-in-wrapper">
+          <h2>Sign In</h2>
+          <InputWithLabel
+            className="email"
+            inputPlaceholder="youremail@example.com"
+            label="Your Email"
+            name="email"
+            onChange={handleEmail}
+            type="email"
+          />
+          <SignInButton label="Sign In" name="sign-in" onClick={handleSignIn} />
+          <SignUpLink href="/sign-up" label="Or Sign Up" />
+          {errors.length ? (
+            <>
+              <p>{'Error(s):'}</p>
+              <ul className="errors">
+                {errors.map(error => (
+                  <li key={error}>{error}</li>
+                ))}
+              </ul>
+            </>
+          ) : null}
+        </div>
+      </div>
+      <style jsx>{`
+        h2 {
+          font-size: 32px;
+          line-height: 35.2px;
+          font-weight: normal;
+        }
+        .box-format {
+          position: relative;
+          max-width: 800px;
+          padding: 10px 40px 64px;
+          margin: 24px auto 0;
+        }
+        .font-format {
+          color: #fff;
+          font-family: 'Work Sans', sans-serif;
+        }
+        .sign-in-wrapper {
+          position: relative;
+          max-width: 650px;
+          margin: 0 auto;
+        }
+      `}</style>
+    </>
+  );
+};
+
+const InputWithLabel = ({
+  className,
+  inputPlaceholder,
+  label,
+  name,
+  onChange,
+  type,
+} = {}) => {
+  return (
+    <>
+      <div className={`input-with-label-wrapper ${className}`}>
+        <label htmlFor={name}>{label}</label>
+        <div className="field-wrapper">
+          <input
+            name={name}
+            onChange={onChange}
+            placeholder={inputPlaceholder}
+            required={true}
+            type={type}
+          />
+        </div>
+      </div>
+      <style jsx>{`
+        label {
+          display: block;
+          font-size: 18px;
+        }
+        input {
+          background-color: ${color.primary};
+          border-radius: 4px;
+          border: 1px solid #FFF;
+          color: #FFF;
+          display: block,
+          font-size: 16px;
+          height: 56px;
+          margin: 12px 0;
+          outline: none;
+          padding: 16px;
+          transition: all 0.2s ease-out;
+          width: 100%;
+        }
+        input:focus {
+              border-color: ${color.secondary} !important;
+            }
+        .field-wrapper {
+          width: 100%;
+          transition: all 0.2s ease-out;
+        }
+      `}</style>
+    </>
+  );
+};
+
+const SignInButton = ({
+  disabled = false,
+  label,
+  loading = false,
+  name,
+  onClick,
+} = {}) => {
+  return (
+    <>
+      <button
+        disabled={disabled || loading}
+        name={name}
+        onClick={onClick}
+        type="submit"
+      >
+        <span className="button-label">{label}</span>
+      </button>
+      <style jsx>{`
+        button {
+          background-color: ${color.button};
+          border-radius: 4px;
+          border: 0;
+          height: 48px;
+          position: relative;
+          width: 100%;
+        }
+        .button-label {
+          font-size: 18px;
+        }
+      `}</style>
+    </>
+  );
+};
+
+const SignUpLink = ({ href, label }) => {
+  return (
+    <>
+      <div className="sign-up">
+        <a href={href}>{label}</a>
+      </div>
+      <style jsx>{`
+        a {
+          text-decoration: underline;
+        }
+        .sign-up {
+          margin-top: 2rem;
+          opacity: 0.8;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default SignInPage;
--- src/test/unit.js
+++ src/test/unit.js
@@ -4,3 +4,4 @@ import '../features/user/reducer.test.js';
 import '../features/user/sign-up.test.js';
 import '../features/user/sign-in.test.js';
 import '../features/sign-up/sign-up-component-test.js';
+import '../features/sign-in/sign-in-component-test.js';
