0a7bd9b1e9988a1f42b5dffb696b7dba143a327f
Oliver Day <oliver.day.swe@gmail.com>
2022-09-06 20:11:14 -0700

    feat: refactor sign in component unit tests have tigher scope

--- src/features/sign-in/sign-in-component-test.js
+++ src/features/sign-in/sign-in-component-test.js
@@ -6,54 +6,61 @@ import match from 'riteway/match';
 import SignIn from './sign-in-component';
 
 describe('Sign In Page', async assert => {
-  const signInTitle = 'Sign In';
-  const emailLabelText = 'Your Email';
-  const emailInputPlaceholder = 'youremail@example.com';
-  const orSignUpLinkText = 'Or Sign Up';
-
   const $ = render(<SignIn />);
   const contains = match($.html());
 
-  assert({
-    given: 'sign in page',
-    should: 'render "Sign In" title',
-    actual: contains(signInTitle),
-    expected: signInTitle,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render email input',
-    actual: $('.email').length,
-    expected: 1,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render "Your email" label text',
-    actual: contains(emailLabelText),
-    expected: emailLabelText,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render email input placeholder text',
-    actual: contains(emailInputPlaceholder),
-    expected: emailInputPlaceholder,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render "Sign In" button',
-    actual: $('[name="sign-in"]').length,
-    expected: 1,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render "Or Sign Up" page link',
-    actual: $('.sign-up').length,
-    expected: 1,
-  });
-  assert({
-    given: 'sign in page',
-    should: 'render "Or Sign Up" link label',
-    actual: contains(orSignUpLinkText),
-    expected: orSignUpLinkText,
-  });
+  {
+    const signInTitle = 'Sign In';
+    assert({
+      given: 'sign in page',
+      should: 'render "Sign In" title',
+      actual: contains(signInTitle),
+      expected: signInTitle,
+    });
+  }
+  {
+    const emailLabelText = 'Your Email';
+    const emailInputPlaceholder = 'youremail@example.com';
+    assert({
+      given: 'sign in page',
+      should: 'render email input',
+      actual: $('.email').length,
+      expected: 1,
+    });
+    assert({
+      given: 'sign in page',
+      should: 'render "Your email" label text',
+      actual: contains(emailLabelText),
+      expected: emailLabelText,
+    });
+    assert({
+      given: 'sign in page',
+      should: 'render email input placeholder text',
+      actual: contains(emailInputPlaceholder),
+      expected: emailInputPlaceholder,
+    });
+  }
+  {
+    assert({
+      given: 'sign in page',
+      should: 'render "Sign In" button',
+      actual: $('[name="sign-in"]').length,
+      expected: 1,
+    });
+  }
+  {
+    const orSignUpLinkText = 'Or Sign Up';
+    assert({
+      given: 'sign in page',
+      should: 'render "Or Sign Up" page link',
+      actual: $('.sign-up').length,
+      expected: 1,
+    });
+    assert({
+      given: 'sign in page',
+      should: 'render "Or Sign Up" link label',
+      actual: contains(orSignUpLinkText),
+      expected: orSignUpLinkText,
+    });
+  }
 });
