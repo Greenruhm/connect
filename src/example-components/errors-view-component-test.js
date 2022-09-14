d4aeb2fa087526baf2a8e702b6a929377bcbff25
Oliver Day <oliver.day.swe@gmail.com>
2022-09-13 17:02:05 -0700

    feat: add skeleton of 'ErrorsView' component

---
+++ src/example-components/errors-view-component-test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { describe } from 'riteway';
+import render from 'riteway/render-component';
+import match from 'riteway/match';
+import ErrorsView from './errors-view-component';
+
+describe('Errors View Component', async assert => {});
---
+++ src/example-components/errors-view-component.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Theme from './theme';
+import SignOutButton from './submit-button-component';
+
+const styles = {};
+
+const ErrorsView = ({ errors } = {}) => {
+  return <>{errors}</>;
+};
+
+export default ErrorsView;
