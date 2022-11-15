eaf80c23a69a71ae522a8bc7f07724e38b0546f6
Oliver Day <oliver.day.swe@gmail.com>
2022-11-15 15:36:18 -0800

    chore: remove unnecessary 'handleMagicError' function

--- src/features/user/with-magic.js
+++ src/features/user/with-magic.js
@@ -1,61 +1,6 @@
 import { Magic } from 'magic-sdk';
 import { ConnectExtension } from '@magic-ext/connect';
 import { ethers } from 'ethers';
-import { createError, errorCauses } from 'error-causes';
-
-export const AuthErrorMessages = {
-  AuthLinkExpired: 'Auth Link Expired',
-  InvalidEmail: 'Invalid Email',
-  InternalError: 'Internal Error',
-  UserRequestEditEmail: 'User Request Edit Email',
-};
-
-// https://magic.link/docs/auth/api-reference/client-side-sdks/web#errors-warnings
-// eslint-disable-next-line no-unused-vars
-const [magicErrors, handleMagicErrors] = errorCauses({
-  AuthLinkExpired: {
-    code: -10001,
-    message: 'Auth Link Expired',
-  },
-  InvalidEmail: {
-    code: -32602,
-    message: 'Invalid Email',
-  },
-  InternalError: {
-    code: -32603,
-    message: 'Internal Error',
-  },
-  UserRequestEditEmail: {
-    code: -10005,
-    message: 'User Request Edit Email',
-  },
-});
-
-export const errorsHandledByConnect = magicErrors;
-
-export const handleMagicError = (error) => {
-  // if MagicLinkExpired
-  if (error.code === magicErrors.AuthLinkExpired.code) {
-    throw createError(magicErrors.AuthLinkExpired);
-  }
-
-  // if InvalidParams
-  if (error.code === magicErrors.InvalidEmail.code) {
-    throw createError(magicErrors.InvalidEmail);
-  }
-
-  // if InternalError
-  if (error.code === magicErrors.InternalError.code) {
-    throw createError(magicErrors.InternalError);
-  }
-
-  // if UserRequestEditEmail
-  if (error.code === magicErrors.UserRequestEditEmail.code) {
-    throw createError(magicErrors.UserRequestEditEmail);
-  }
-
-  return;
-};
 
 const withMagic = (params) => {
   const options = {
@@ -67,11 +12,9 @@ const withMagic = (params) => {
   return {
     ...params,
     magic,
-    handleMagicError,
   };
 };
 
-// TODO: Oliver remove scratch work
 export const withMagicConnect = (params) => {
   const magic = new Magic('pk_live_BDB8311A26CF3651', {
     extensions: [new ConnectExtension()],
@@ -82,12 +25,9 @@ export const withMagicConnect = (params) => {
 
   return {
     ...params,
-    handleMagicError,
     magic,
     web3Provider,
   };
 };
-
-export { magicErrors, handleMagicErrors };
 
 export default withMagic;
