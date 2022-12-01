a18cc10a0cd66768b96bb2b63c8600e1c58e50fe
Oliver Day <oliver.day.swe@gmail.com>
2022-12-01 14:49:49 -0800

    refactor: remove error codes from example sign in components

--- src/example-components/sign-in-connect/sign-in-controller-component.js
+++ src/example-components/sign-in-connect/sign-in-controller-component.js
@@ -3,9 +3,6 @@ import React, { useState } from 'react';
 import connect from '../..';
 import SignInView from './sign-in-view-component';
 
-// https://connect.greenruhm.com/fundamentals/user-accounts#sign-in
-const errorsHandledByConnect = [-10001, -32602, -32603, -10005];
-
 const SignInController = ({
   authStatus: initialAuthStatus = 'Signed Out',
 } = {}) => {
@@ -17,10 +14,22 @@ const SignInController = ({
   });
   const { authStatus, email, errors, username } = state;
 
-  const { signIn, signOut } = connect({
+  const { signIn, handleSignInErrors, signOut } = connect({
     apiKey: '<your-api-key>',
     features: ['magic-connect'],
   });
+
+  const setAuthStatusToSignedOut = () =>
+    setState((state) => ({
+      ...state,
+      authStatus: 'Signed Out',
+    }));
+
+  const setErrorMessage = (message) =>
+    setState((state) => ({
+      ...state,
+      errors: [...state.errors, message],
+    }));
 
   const clearErrors = (e) => {
     setState((state) => ({
@@ -29,31 +38,49 @@ const SignInController = ({
     }));
   };
 
+  const handleSignInSuccess = (userData) =>
+    setState((state) => ({
+      ...state,
+      authStatus: 'Signed In',
+      email: userData.email,
+      username: userData.username,
+    }));
+
   const handleSignIn = async () => {
     try {
       setState((state) => ({
         ...state,
         authStatus: 'Signing In',
       }));
-      const userData = await signIn();
-      setState((state) => ({
-        ...state,
-        authStatus: 'Signed In',
-        email: userData.email,
-        username: userData.username,
-      }));
+      await signIn()
+        .then(handleSignInSuccess)
+        .catch(
+          handleSignInErrors({
+            AccountNotFound: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            AuthInternalError: () => setAuthStatusToSignedOut(),
+            AuthInvalidEmail: () => setAuthStatusToSignedOut(),
+            AuthLinkExpired: () => setAuthStatusToSignedOut(),
+            AuthUserRequestEditEmail: () => setAuthStatusToSignedOut(),
+            AuthUserRejectedConsentToShareEmail: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            EmailIsRequired: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            InternalServerError: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+          })
+        );
     } catch (e) {
-      // if error has NOT already been handled by Connect UI
-      if (!errorsHandledByConnect.includes(e?.cause?.code)) {
-        setState((state) => ({
-          ...state,
-          errors: [...state.errors, e.message],
-        }));
-      }
-      setState((state) => ({
-        ...state,
-        authStatus: 'Signed Out',
-      }));
+      setErrorMessage(e.message);
+      setAuthStatusToSignedOut();
     }
   };
 
@@ -61,15 +88,9 @@ const SignInController = ({
     try {
       await signOut();
     } catch (e) {
-      setState((state) => ({
-        ...state,
-        errors: [...state.errors, e.message],
-      }));
+      setErrorMessage(e.message);
     }
-    setState((state) => ({
-      ...state,
-      authStatus: 'Signed Out',
-    }));
+    setAuthStatusToSignedOut();
   };
 
   return (
--- src/example-components/sign-in/sign-in-component.js
+++ src/example-components/sign-in/sign-in-component.js
@@ -7,9 +7,6 @@ import SignInButton from '../shared/submit-button-component';
 import SuccessView from '../shared/success-view';
 import ErrorModal from '../shared/error-modal';
 
-// https://connect.greenruhm.com/fundamentals/user-accounts#sign-in
-const errorsHandledByConnect = [-10001, -32602, -32603, -10005];
-
 const styles = {
   a: {
     textDecoration: 'underline',
@@ -100,7 +97,9 @@ const renderView = ({
         username,
       });
 
-const { signIn, signOut } = connect({ apiKey: '<your-api-key>' });
+const { signIn, handleSignInErrors, signOut } = connect({
+  apiKey: '<your-api-key>',
+});
 
 const SignInPage = ({ authStatus: initialAuthStatus = 'Signed Out' } = {}) => {
   const [state, setState] = useState({
@@ -110,6 +109,18 @@ const SignInPage = ({ authStatus: initialAuthStatus = 'Signed Out' } = {}) => {
     username: '',
   });
   const { authStatus, email, errors, username } = state;
+
+  const setAuthStatusToSignedOut = () =>
+    setState((state) => ({
+      ...state,
+      authStatus: 'Signed Out',
+    }));
+
+  const setErrorMessage = (message) =>
+    setState((state) => ({
+      ...state,
+      errors: [...state.errors, message],
+    }));
 
   const clearErrors = (e) => {
     setState((state) => ({
@@ -125,48 +136,59 @@ const SignInPage = ({ authStatus: initialAuthStatus = 'Signed Out' } = {}) => {
     }));
   };
 
+  const handleSignInSuccess = (userData) =>
+    setState((state) => ({
+      ...state,
+      authStatus: 'Signed In',
+      email: userData.email,
+      username: userData.username,
+    }));
+
   const handleSignIn = async () => {
     try {
       setState((state) => ({
         ...state,
         authStatus: 'Signing In',
       }));
-      const userData = await signIn({ email, username });
-      setState((state) => ({
-        ...state,
-        authStatus: 'Signed In',
-        email: userData.email,
-        username: userData.username,
-      }));
+      await signIn({ email, username })
+        .then(handleSignInSuccess)
+        .catch(
+          handleSignInErrors({
+            AccountNotFound: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            AuthInternalError: () => setAuthStatusToSignedOut(),
+            AuthInvalidEmail: () => setAuthStatusToSignedOut(),
+            AuthLinkExpired: () => setAuthStatusToSignedOut(),
+            AuthUserRequestEditEmail: () => setAuthStatusToSignedOut(),
+            AuthUserRejectedConsentToShareEmail: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            EmailIsRequired: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+            InternalServerError: ({ message }) => {
+              setErrorMessage(message);
+              setAuthStatusToSignedOut();
+            },
+          })
+        );
     } catch (e) {
-      console.log({ e });
-      // if error has NOT already been handled by Connect UI
-      if (!errorsHandledByConnect.includes(e?.cause?.code)) {
-        setState((state) => ({
-          ...state,
-          errors: [...state.errors, e.message],
-        }));
-      }
-      setState((state) => ({
-        ...state,
-        authStatus: 'Signed Out',
-      }));
+      setErrorMessage(e.message);
+      setAuthStatusToSignedOut();
     }
   };
 
   const handleSignOut = async () => {
     try {
-      await signOut({ email });
+      await signOut();
     } catch (e) {
-      setState((state) => ({
-        ...state,
-        errors: [...state.errors, e.message],
-      }));
+      setErrorMessage(e.message);
     }
-    setState((state) => ({
-      ...state,
-      authStatus: 'Signed Out',
-    }));
+    setAuthStatusToSignedOut();
   };
 
   const disabled = !isValidEmail(email);
