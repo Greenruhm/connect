287b82384fca26108f53a9111677416a728249dd
Bruno Moutinho <bmoutinho@protonmail.com>
2023-07-17 14:34:30 -0300

    fix: style for loaded component

--- src/components/shared/loading/index.js
+++ src/components/shared/loading/index.js
@@ -9,9 +9,32 @@ function Loading({ small, ...props }) {
     wrapperRef.current.style.opacity = 1;
   }, []);
 
+  const styles = {
+    circle: {
+      height: small ? '72px' : '120px',
+      width: '100%',
+    },
+    logo: {
+      height: small ? '16px' : '26px',
+      width: '100%',
+      marginLeft: '-100%',
+    },
+    wrapper: {
+      display: 'flex',
+      alignItems: 'center',
+      opacity: 0,
+      transition: 'opacity 0.15s ease-in',
+    },
+  };
+
   return (
-    <div ref={wrapperRef} className="load-spinner wrapper" {...props}>
-      <svg className="circle" viewBox="0 0 100 100">
+    <div
+      ref={wrapperRef}
+      className="load-spinner"
+      style={styles.wrapper}
+      {...props}
+    >
+      <svg className="circle" viewBox="0 0 100 100" style={styles.circle}>
         <path
           fill="#49ECBD"
           d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
@@ -30,6 +53,7 @@ function Loading({ small, ...props }) {
 
       <svg
         className="logo"
+        style={styles.logo}
         width="24"
         height="24"
         viewBox="0 0 64 64"
@@ -43,23 +67,6 @@ function Loading({ small, ...props }) {
           fill="#49ECBD"
         />
       </svg>
-      <style jsx>{`
-        div.wrapper {
-          display: flex;
-          align-items: center;
-          opacity: 0;
-          transition: opacity 0.15s ease-in;
-        }
-        svg.circle {
-          height: ${small ? '72px' : '120px'};
-          width: 100%;
-        }
-        svg.logo {
-          height: ${small ? '16px' : '26px'};
-          width: 100%;
-          margin-left: -100%;
-        }
-      `}</style>
     </div>
   );
 }
